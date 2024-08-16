21f8590056a19a2c781450193045c18733f8dc30
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:23:32 +0300

    fix com.qualcomm.qti.dpm.api@1.0 ELF checks
    Change-Id: I759752ccc133d7047d415eb83c5d3c55820437a4

--- mata-vendor.mk
+++ mata-vendor.mk
@@ -470,7 +470,7 @@ PRODUCT_PACKAGES += \
     libsmwrapper \
     libvideobokeh \
     libvqzip \
-    com.qualcomm.qti.dpm.api@1.0 \
+    com.qualcomm.qti.dpm.api@1.0_vendor \
     com.qualcomm.qti.imscmservice@2.0 \
     com.qualcomm.qti.imscmservice@2.1 \
     com.qualcomm.qti.imscmservice@2.2 \
